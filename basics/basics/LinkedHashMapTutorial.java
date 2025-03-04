6cea811a49c93484798cf7f002d90381c0a8e932
Farmix2003 <ffarrux386@gmail.com>
2025-03-04 21:46:01 +0200

    TreeMap constructor creations

--- basics/basics/LinkedHashMapTutorial.java
+++ basics/basics/LinkedHashMapTutorial.java
@@ -1,10 +1,35 @@
 package basics;
 
+import java.util.LinkedHashMap;
+
+
+//Java LinkedHashMap class is Hashtable and Linked list implementation of the Map interface,
+//with predictable iteration order.
+//It inherits HashMap class and implements the Map interface.
+//
+//Points to remember
+//Java LinkedHashMap contains values based on the key.
+//Java LinkedHashMap contains unique elements.
+//Java LinkedHashMap may have one null key and multiple null values.
+//Java LinkedHashMap is non synchronized.
+//Java LinkedHashMap maintains insertion order.
+//The initial default capacity of Java HashMap class is 16 with a load factor of 0.75
+
+
 public class LinkedHashMapTutorial {
    
 	public static void main(String[] args) {
 		
-		Linked
+		LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
+		
+		map.put(1, "Jhon");
+		map.put(2, "Lucas");
+		map.put(3, "Ron");
+		map.put(4, "Joel");
+		map.put(5, "Mason");
+		
+		//All methods of LinkedHashMap are the same with methods of HashMap
+		
 		
 	}
 }
---
+++ basics/basics/TreeMapTutorial.java
@@ -0,0 +1,43 @@
+package basics;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+//Java TreeMap class is a red-black tree-based implementation.
+//It provides an efficient means of storing key-value pairs in sorted order.
+
+//The java.util package contains the Java TreeMap class, which is a component of the Java Collections Framework.
+//It extends the AbstractMap class and implements the NavigableMap interface. 
+//TreeMap is an effective red-black tree-based solution that sorts key-value pairs.
+//TreeMap works well in situations where ordered key-value pairs are necessary since it preserves ascending order.
+//It also only includes distinct components, guaranteeing that every key corresponds to a single value.
+//TreeMap can have more than one null value, even if it cannot have a null key.
+
+//Important Points about TreeMap:
+
+//Java TreeMap contains values based on the key. It implements the NavigableMap interface and extends AbstractMap class.
+//Java TreeMap contains only unique elements.
+//Java TreeMap cannot have a null key but can have multiple null values.
+//Java TreeMap is non synchronized.
+//Java TreeMap maintains ascending order.
+
+public class TreeMapTutorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		//Creation of TreeMap:
+		
+		//Constructing an empty tree map that will be sorted using the natural order of its key.
+		Map<Integer, String> treeMap = new TreeMap<>();
+		
+        //Constructing an empty tree-based map that will be sorted using the comparator comp.
+		Map<Integer, Integer> map = new TreeMap<>(Comparator.comparing(k -> k > 2));
+		
+		Map<Integer, Integer> map2 = new TreeMap<>(Map.of(1,2,3,4,5,6,7,8,9,10, 3,2));
+		System.out.println(map2);
+		
+	}
+
+}
